f67567a6cfe649eb352a38e916a55eceb800d7ca
Wardi Utari <wardi.utari@gmail.com>
2020-01-07 09:38:53 +0700

    Ad Second part of sholah time
    add 2 function
    - getTimePart with secondPart output
    - getTimeString_long Sring output with secondPart

--- SholahTimeCalulation.h
+++ SholahTimeCalulation.h
@@ -1,7 +1,5 @@
-
 #ifndef SholahTimeCalulation_h
 #define SholahTimeCalulation_h
-
 
 #define d2r(x) x*M_PI/180
 #define r2d(x) x*180/M_PI
@@ -19,20 +17,18 @@ enum SholahID
     SholatCount
   };
   
-
-
 class SholahTimeCalulation
   {
     public    :
 		float sholahTime[SholatCount];
-		char  sholahName[8][10]  =     {"Imsak", 
-						"Subuh",
-						"Terbit",
-						"Dhuha",
-						"Dzuhur",
-						"Ashar",
-						"Maghrib",
-						"Isya"};
+		char  sholahName[8][10]  ={"Imsak", 
+									"Subuh",
+									"Terbit",
+									"Dhuha",
+									"Dzuhur",
+									"Ashar",
+									"Maghrib",
+									"Isya"};
 	  
 		SholahTimeCalulation(float latitude, float longitude, float elevation,int timezone);
 		SholahTimeCalulation(float latitude, float longitude, int timezone);
@@ -43,11 +39,14 @@ class SholahTimeCalulation
 		void 	dateSet(int year,int month,int date);
 		void 	ihtiyatiSet(int ihtiyati);
 		void 	calculate();
+		void 	calculate(float latitude, float longitude, float elevation,int timezone,int year,int month,int date);
 		void 	getTimePart(int idShol,int& hourPart, int& minutePart);
+		void 	getTimePart(int idShol,int& hourPart, int& minutePart,int& secondPart);
 		void 	getTimeString(int idShol,char* stringTime);
+		void 	getTimeString_long(int idShol,char* stringTime);
       
     protected:
-		float _latitude   	= -6.175513;	//Default Coordinat --> Jakarta (Monas - National Monument)	
+		float _latitude   	= -6.175513;		//Default Coordinat --> Jakarta (Monas - National Monument)	
 		float _longitude  	= 106.827170;
 		float _elevation 	= 0; 			//Default Elevation --> 0 (Sea Level)
 		int   _timezone   	= 7;			//Default Time Zone +7 (Jakarta West Part of Indonesia)
